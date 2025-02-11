c482d2bf893afe1651a041c7cc2b59db52b3c359
prathamdmehta <mehta2935@gmail.com>
2025-02-11 18:28:44 +0530

    updateit

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { specialityData } from '../assets/assets';
 import { Link } from 'react-router-dom';
 import { gsap } from 'gsap';
